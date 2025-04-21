c8e0a28c1a7b45afa7129e28cd5dc909b7497f21
João Pedro <joaomelanes12@gmail.com>
2025-04-21 12:13:08 -0300

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+// components
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
+import Container from './components/layouts/Container'
+
 // Pages
 import Login from './components/pages/auth/Login';
 import Register from './components/pages/auth/Register';
 import Home from './components/pages/Home';
 
 
+// contexts
+import { UserProvider } from './context/UserContext';
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/login'>
-          <Login />
-        </Route>
-        <Route path='/register'>
-          <Register />
-        </Route>
-        <Route path='/'>
-          <Home />
-        </Route>
-      </Routes>
-    </Router>
+      <Router>
+        <UserProvider>
+          <Navbar />
+            <Container>
+              <Routes>
+                <Route path='/login' element={<Login />} />
+                <Route path='/' element={<Home />} />
+                <Route path='/register' element={<Register />} />
+              </Routes>
+            </Container>
+          <Footer />
+        </UserProvider>
+      </Router>
   );
 }
 
--- frontend/src/components/pages/auth/Register.js
+++ frontend/src/components/pages/auth/Register.js
@@ -1,9 +1,78 @@
+import Input from "../../form/Input"
+import styles from "../../form/Form.module.css"
+import {Link} from 'react-router-dom'
+import { useContext, useState } from "react"
+
+// context
+import { Context } from "../../../context/UserContext"
+
 function Register(){
+
+    const [user, setUser] = useState({})
+    const {register} = useContext(Context)
+
+    function handleChange(e){
+        setUser({...user, [e.target.name]: e.target.value})
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+
+        // enviar usuario para o banco
+        register(user)
+    }
+
     return(
-        <section>
+        <section className={styles.form_container}>
             <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+                <Input 
+                    text="Nome"
+                    type="text"
+                    name="name"
+                    placeholder="Digite seu nome"
+                    handleOnChange={handleChange}
+                />
+
+                <Input 
+                    text="Telefone"
+                    type="text"
+                    name="phone"
+                    placeholder="Digite seu telefone"
+                    handleOnChange={handleChange}
+                />
+
+                <Input 
+                    text="E-mail"
+                    type="email"
+                    name="email"
+                    placeholder="Digite seu email"
+                    handleOnChange={handleChange}
+                />
+
+                <Input 
+                    text="Senha"
+                    type="password"
+                    name="password"
+                    placeholder="Digite a senha"
+                    handleOnChange={handleChange}
+                />
+
+                <Input 
+                    text="Confirmação de senha"
+                    type="password"
+                    name="confirmpassword"
+                    placeholder="Confirme a senha"
+                    handleOnChange={handleChange}
+                />
+
+                <input type="submit" value="Cadastrar" />
+                <p>
+                    Já tem conta?<Link to="/login">Clink Aqui.</Link>
+                </p>
+            </form>
         </section>
-    );
+    )
 }
 
 export default Register
